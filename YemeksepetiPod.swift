37a3001cc1c13bf1ed619c5e069d0a4e54364122
kerimcaglar <kerim.caglarr@gmail.com>
2021-07-11 13:24:15 +0300

    Added tests

---
+++ YemeksepetiPod.swift
@@ -0,0 +1,21 @@
+//
+//  YemeksepetiPod.swift
+//  YemekSepetiPod
+//
+//  Created by Kerim Caglar on 11.07.2021.
+//
+
+import Foundation
+
+class YemeksepetiPod {
+    
+    let name = "Yemeksepeti & Patika Bootcamp"
+    
+    func sum(x:Int, y:Int) -> Int {
+        return x + y
+    }
+    
+    func multiply(x:Int, y:Int) -> Int {
+        return x * y
+    }
+}
