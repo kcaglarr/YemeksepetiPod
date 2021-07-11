89ab9848aa764400941c4af4b208a8c24397309d
kerimcaglar <kerim.caglarr@gmail.com>
2021-07-11 13:10:42 +0300

    Initial commit

---
+++ YemekSepetiPod/YemekSepetiPod.h
@@ -0,0 +1,18 @@
+//
+//  YemekSepetiPod.h
+//  YemekSepetiPod
+//
+//  Created by Kerim Caglar on 11.07.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YemekSepetiPod.
+FOUNDATION_EXPORT double YemekSepetiPodVersionNumber;
+
+//! Project version string for YemekSepetiPod.
+FOUNDATION_EXPORT const unsigned char YemekSepetiPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YemekSepetiPod/PublicHeader.h>
+
+
